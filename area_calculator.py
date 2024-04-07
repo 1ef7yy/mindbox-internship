739965266c3709935310881848d13147fc6283b3
leftyy <ustyantsev.makar@mail.ru>
2024-04-07 17:07:49 +0300

    added compile-time area calculation and right triangle check

--- area_calculator.py
+++ area_calculator.py
@@ -1,10 +1,59 @@
-import math
+from math import pi, sqrt
 
-class AreaCalculator:
-    def calculate_circle_area(self, radius: float) -> float:
-        return math.pi * radius ** 2
+class Shape:
+    def calculate_area(self):
+        raise NotImplementedError("Define the shape!")
     
-    def calculate_triangle_area(self, sides: list[float]) -> float:
-        p = sum(sides) / 2
-        return math.sqrt(p*(p-sides[0])*(p-sides[1])*(p-sides[2]))
+
+
+class Circle(Shape): # класс круга
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return pi * self.radius ** 2
     
+
+class Triangle(Shape): # класс треугольника
+    def __init__(self, sides:list[float]):
+        self.sides = sorted(sides)
+        self.a = sides[0]
+        self.b = sides[1]
+        self.c = sides[2]
+
+        if self.a + self.b < self.c:
+            raise ValueError("Such triangle does not exist")
+        
+
+    def calculate_area(self) -> float:
+        p = self.sides / 2
+        return sqrt(p*(p-self.a)*(p-self.b)*(p-self.c))
+    
+    def is_right(self) -> bool:
+        return self.c**2 == self.b**2 + self.a**2
+    
+
+
+class Rectangle(Shape): # класс прямоугольника
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self) -> float:
+        return self.width * self.height
+    
+
+
+def calculate_area_all(*args):
+    if len(args) == 1:
+        return Circle(*args).calculate_area()
+    
+    elif len(args) == 2:
+        return Rectangle(*args).calculate_area()
+    
+    elif len(args) == 3:
+        return Triangle([*args]).calculate_area()
+    
+    return ValueError("No such shape!")
+
+
